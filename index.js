d20c8c0a9d31405a5d0fae2e554dc9bc48782a0d
Marek Ciążyński <m.ciazynski@bfirst.tech>
2021-02-14 18:49:17 +0100

    Basic login implementation

--- index.js
+++ index.js
@@ -1,6 +1,102 @@
 'use strict';
 import './style.css'
 
+/**
+ * Shuffles array in place. ES6 version
+ * @param {Array} a items An array containing the items.
+ */
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
+
+let _latexFormulas = [
+	'\\int_{-\\infty}^\\infty\\f\\hat\\xi\\,e^{2 \\pi i \\xi x}\\,d\\xi',
+	'\\int_{-\\infty}^\\infty\\f\\xi\\,\pi^{3 \\pi i \\xi x}\\,d\\xi',
+	'\\int_{a}^b\\f\\xi\\,e^{2 \\pi i \\xi x}\\,d\\xi',
+	'x^n + y^n = z^n',
+	'E=mc^2',
+	'\\frac{n!}{k!(n-k)!} = \\binom{n}{k}',
+];
+let latexFormulas = [1,2,3,4,5,6].map(x => x.toString());
+latexFormulas = latexFormulas.concat(latexFormulas);
+// shuffle(latexFormulas);
+console.log(latexFormulas);
+
+let cardSelection = [];
+let cardsLeft = 12;
+
+function resetAllCards() {
+	const cardContents = document.querySelectorAll('.card-content');
+	for (const card of cardContents) {
+		if (!card.classList.contains('revealed'))
+			card.classList.remove('flip');
+	}
+	lock = false;
+}
+
+let lock = false;
+const onCardSelected = (event) => {
+	if (lock)
+		return false;
+	
+	const selectedCard = event.target;
+	cardSelection.push(selectedCard);
+	selectedCard.classList.add('flip');
+
+	// selectedCard.classList.add('flip');
+	console.log(cardSelection.map(x => x.dataset.latex))
+
+	if (cardSelection.length >= 2) {
+		if (cardSelection[0].dataset.latex === cardSelection[1].dataset.latex) {
+			cardSelection.forEach(x => x.classList.add('revealed'));
+			cardsLeft -= 2;
+			if (cardsLeft <= 0) {
+				alert('Hooray!');
+			}
+		} else {
+			lock = true;
+			setTimeout(resetAllCards, 1000);
+		}
+		cardSelection = [];
+	} else if (cardSelection[0] === cardSelection[1]) {
+		return false;
+	}
+
+	// cardSelection.push(selectedCard);
+	// selectedCard.classList.add('flip');
+
+	// console.log(cardSelection, cardSelection.length)
+	// selectedCard.classList.toggle('flip');
+};
+
 document.addEventListener("DOMContentLoaded", function (event) {
-	document.querySelectorAll('');
+	const cardContents = document.querySelectorAll('.card-content');
+
+	let formulaIndex = 0;
+	for (const card of cardContents) {
+		const cardBackface = card.querySelector('.back');
+
+		const formula = latexFormulas[formulaIndex++]
+		card.dataset.latex = formula;
+
+		katex.render(formula, cardBackface, {
+			displayMode: false,
+			leqno: false,
+			fleqn: false,
+			throwOnError: true,
+			errorColor: "#cc0000",
+			strict: "warn",
+			output: "htmlAndMathml",
+			trust: false,
+			macros: { "\\f": "#1f(#2)" }
+		});
+	}
+
+	for (const card of cardContents) {
+		card.addEventListener('click', onCardSelected);
+	}
 });
